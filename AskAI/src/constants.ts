105eb361c0c73fdaebaaf52e48e32daf6bfb9c19
c4605 <bolasblack@gmail.com>
2025-12-04 22:17:09 +0100

    feat(AskAI): update default model name

--- AskAI/src/constants.ts
+++ AskAI/src/constants.ts
@@ -1,4 +1,4 @@
 // export const DEFAULT_MODEL = "gpt-4o-mini" as const;
 // export const MODELS = ["gpt-3.5-turbo", "gpt-4", "gpt-4o", "gpt-4o-mini"] as const;
-export const DEFAULT_MODEL = "claude-3-5-sonnet-latest";
-export const MODELS = ["claude-3-5-sonnet-latest"];
+export const DEFAULT_MODEL = "claude-sonnet-4-5";
+export const MODELS = ["claude-sonnet-4-5"];
